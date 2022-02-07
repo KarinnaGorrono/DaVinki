ca56595cbfb03efa6e25b09df9231ccf0123664a
SamanthaDison <samnicdis@gmail.com>
2022-02-07 11:32:07 -0700

    followers service done i think

--- DaVinki.client/src/AppState.js
+++ DaVinki.client/src/AppState.js
@@ -5,5 +5,7 @@ export const AppState = reactive({
   user: {},
   account: {},
   pieces: [],
-  activePiece: {}
+  activePiece: {},
+  followers: [],
+  activeFollower: {}
 })
---
+++ DaVinki.client/src/services/FollowersService.js
@@ -0,0 +1,32 @@
+import { AppState } from "../AppState"
+import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
+
+class FollowersService {
+    async getByAccount(accountId) {
+        const active = AppState.followers.find(f => f.id === accountId)
+        logger.log('active', active)
+        AppState.activeFollower = active
+    }
+
+    async createFollower() {
+        const res = await api.post('api/followers', body)
+        logger.log(res.data)
+        AppState.followers.unshift(res.body)
+        return res.data
+    }
+    async removeFollower(id) {
+        const res = await api.delete('api/followers/' + id)
+        logger.log(res.data)
+        const found = AppState.followers.findIndex(f => f.id == id)
+        AppState.followers.splice(found, 1)
+        logger.log(AppState.followers)
+    }
+
+
+}
+
+
+
+
+export const followersService = new FollowersService()

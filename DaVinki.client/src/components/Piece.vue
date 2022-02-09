c2e2f1a02f6b14ff20b6d472b36097c12a4d257e
SamanthaDison <samnicdis@gmail.com>
2022-02-09 10:00:05 -0700

    fixed where nested router is on account page

--- DaVinki.client/src/components/Piece.vue
+++ DaVinki.client/src/components/Piece.vue
@@ -4,41 +4,37 @@
     :data-bs-target="'#piece-' + piece.id"
     data-bs-toggle="modal"
   >
-    <img
-      :src="piece.coverImg"
-      class=" w-100 selectable elevation-3"
-    
-    />
+    <img :src="piece.coverImg" class="w-100 selectable" />
     <div class="piece-body pb-4 text-uppercase pt-1">
       <h4 class="piece-title">{{ piece.name }}</h4>
       <h6 class="piece-artist">{{ piece.artist.name }}</h6>
     </div>
-  <Modal :id="'piece-' + piece.id">
-    <template #modal-title>
-      <h4>{{ piece.name }}</h4>
-    </template>
-    <template #modal-body>
-      <div class="row">
-        <div class="col-md-3">
-          <div class="pt-5">
-            <p>{{ piece.artist }}</p>
+    <Modal :id="'piece-' + piece.id">
+      <template #modal-title>
+        <h4>{{ piece.name }}</h4>
+      </template>
+      <template #modal-body>
+        <div class="row">
+          <div class="col-md-3">
+            <div class="pt-5">
+              <p>{{ piece.artist }}</p>
+            </div>
+          </div>
+          <div class="col-md-9 d-flex justify-content-center pt-3">
+            <img
+              class="rounded img-fluid details-img"
+              :src="piece.coverImg"
+              alt=""
+            />
           </div>
         </div>
-        <div class="col-md-9 d-flex justify-content-center pt-3">
-          <img
-            class="rounded img-fluid details-img"
-            :src="piece.coverImg"
-            alt=""
-          />
+        <div class="row pt-5">
+          <p class="">
+            {{ piece.description }}
+          </p>
         </div>
-      </div>
-      <div class="row pt-5">
-        <p class="">
-          {{ piece.description }}
-        </p>
-      </div>
-    </template>
-  </Modal>
+      </template>
+    </Modal>
   </div>
 </template>
 
--- DaVinki.client/src/pages/AccountPage.vue
+++ DaVinki.client/src/pages/AccountPage.vue
@@ -8,9 +8,16 @@
       <h1>{{ account.name }}</h1>
     </div>
     <div class="row">
-      <div class="col-6 d-flex justify-content-around">
-        <h2 class="text-center">Incoming Commissions</h2>
-        <h2 class="text-center">My Commissions</h2>
+      <div class="col-6">
+        <div class="row">
+          <div class="col-12 d-flex justify-content-around">
+            <h2 class="text-center">Incoming Commissions</h2>
+            <h2 class="text-center">My Commissions</h2>
+          </div>
+        </div>
+        <div class="row">
+          <router-view />
+        </div>
       </div>
       <div class="col-6">
         <div class="my-2 d-flex">
@@ -18,7 +25,6 @@
         </div>
         <div></div>
         <div class="d-flex justify-content-end">
-          <router-view />
           <button
             type="button"
             data-bs-toggle="modal"

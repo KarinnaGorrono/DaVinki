d1f93bf6a1cba9d7515bf2c246949660c6727de0
Kyle Evans <krevan88@protonmail.com>
2022-02-09 11:26:08 -0700

    abstract firebase keys to env file

--- DaVinki.client/src/env.js
+++ DaVinki.client/src/env.js
@@ -5,3 +5,12 @@ export const domain = 'dev-b7ths2an.us.auth0.com'
 export const clientId = 'glXQ2lBOMkhnGGdXTJNchjGebJJK9yvt'
 export const audience = 'https://DaVinki.com'
 
+export const firebaseConfig = {
+  apiKey: "AIzaSyCpWVCOTxJHg-_X3UUaUe-tg8PYHFBXQO4",
+  authDomain: "davinki-c6a76.firebaseapp.com",
+  projectId: "davinki-c6a76",
+  storageBucket: "davinki-c6a76.appspot.com",
+  messagingSenderId: "837566518810",
+  appId: "1:837566518810:web:a4d733e3823e6bf33ae3a3",
+  measurementId: "G-P8RJLLGD55"
+};
--- DaVinki.client/src/services/FirebaseService.js
+++ DaVinki.client/src/services/FirebaseService.js
@@ -3,20 +3,9 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 import { getStorage, ref, uploadBytes } from "firebase/storage";
 import { AppState } from "../AppState";
+import { firebaseConfig } from "../env";
 import Pop from "../utils/Pop";
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCpWVCOTxJHg-_X3UUaUe-tg8PYHFBXQO4",
-  authDomain: "davinki-c6a76.firebaseapp.com",
-  projectId: "davinki-c6a76",
-  storageBucket: "davinki-c6a76.appspot.com",
-  messagingSenderId: "837566518810",
-  appId: "1:837566518810:web:a4d733e3823e6bf33ae3a3",
-  measurementId: "G-P8RJLLGD55"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

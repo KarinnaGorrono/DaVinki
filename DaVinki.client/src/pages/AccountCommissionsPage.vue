ef460afb146daccd714acc3688863a781b7d102c
SamanthaDison <samnicdis@gmail.com>
2022-02-09 16:21:24 -0700

    working on commissions

--- DaVinki.client/src/pages/AccountCommissionsPage.vue
+++ DaVinki.client/src/pages/AccountCommissionsPage.vue
@@ -1,21 +1,43 @@
 <template>
-  <p>This is the account commissions page</p>
+  <div class="row">
+    <div class="col-md-12 d-flex justify-content-around">
+      <table class="">
+        <tr>
+          <th>Piece</th>
+          <th>Deadline</th>
+          <th>Status</th>
+        </tr>
+        <tr>
+          <td>Commission Name</td>
+          <td>Commission Deadline</td>
+          <td>Commission Status</td>
+        </tr>
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
-import { onMounted } from '@vue/runtime-core';
+import { computed, onMounted } from '@vue/runtime-core';
 import { logger } from '../utils/Logger';
+import { commissionsService } from '../services/CommissionsService'
+import { AppState } from '../AppState';
+import { AuthService } from '../services/AuthService';
 export default {
   setup() {
     onMounted(async () => {
       try {
-        await commissionsService.getCommission(req.userInfo.id)
+        await commissionsService.getCommission(AuthService.userInfo.id)
         console.log('getting commissions')
       } catch (error) {
         logger.log(error)
       }
     })
-    return {};
+    return {
+      user: computed(() => AppState.user),
+      account: computed(() => AppState.account),
+      commissions: computed(() => AppState.commissions)
+    };
 
   }
 

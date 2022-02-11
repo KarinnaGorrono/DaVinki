000dcc08c5906c92f8cc3b1510adf253178858d3
SamanthaDison <samnicdis@gmail.com>
2022-02-11 15:55:48 -0700

    push to fix

--- DaVinki.client/src/services/CommissionsService.js
+++ DaVinki.client/src/services/CommissionsService.js
@@ -6,7 +6,7 @@ class CommissionsService {
     async getCommission(artistId) {
         const res = await api.get('api/commissions', artistId)
         logger.log('active', res.data)
-        AppState.commissions = res.data
+        AppState.myCommissions = res.data
     }
 
     async createCommission(body) {
